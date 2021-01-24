814efe714993673be81597d6e76ae82dc958a919
Florian Rampin <45884478+rampinflorian@users.noreply.github.com>
2021-01-24 21:06:02 +0100

    Add: ContactForm

---
+++ MaPagePerso.net/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using MaPagePerso.net.Form;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+
+namespace MaPagePerso.net.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("contact/mailer", Name = "Mailer")]
+        public IActionResult Mailer([Bind("Username,Mail,Content")] Mailer mailer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            var email = new MimeMessage();
+            
+            email.From.Add(MailboxAddress.Parse("contact@florianrampin.fr"));
+            email.To.Add(MailboxAddress.Parse("contact@florianrampin.fr"));
+
+            email.Subject = $"FlorianRampin.fr - Nouveau contact : {mailer.Username}";
+            
+            var toto = mailer;
+            return RedirectToAction("Index");
+        }
+        
+        
+    }
+}
---
+++ MaPagePerso.net/Form/Mailer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaPagePerso.net.Form
+{
+    [NotMapped]
+    public class Mailer
+    {
+        [Required]
+        public string Username { get; set; }
+        [EmailAddress, Required]
+        public string Mail { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
