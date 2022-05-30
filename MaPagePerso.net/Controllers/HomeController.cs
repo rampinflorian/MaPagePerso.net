6b371447ebcbffcb0073b49ead8780b805c2f7b5
Florian Rampin <contact@florianrampin.fr>
2022-05-30 13:51:59 +0200

    rework started

--- MaPagePerso.net/Controllers/HomeController.cs
+++ MaPagePerso.net/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MaPagePerso.net.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MaPagePerso.net.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaPagePerso.net.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var projects = await _context.Projects.OrderByDescending(m => m.CreatedAt).ToListAsync();
+            return View(projects);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
